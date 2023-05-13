d65e30e9f4d85d071d2f73656f95c2e765da4492
bianca <bianca.g.buha@gmail.com>
2023-05-13 13:31:58 +0100

    uuid added

--- my-app/src/Components/Dictionary/Dictionary.js
+++ my-app/src/Components/Dictionary/Dictionary.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { v4 as uuidv4 } from "uuid";
+
 function Dictionary(props) {
     return (
         <div className= "meaning-area">
             {props.typeDefs.length > 0 &&
       props.typeDefs[0].type.map((type, index) => (
-        <div className="all-defs" key={index}>
+        <div className="all-defs" key={uuidv4()}>
           <h3 className= "type">{type}</h3>
           <h4 className = "definitions">Definitions</h4>
           <ul className= "definitions-list">
             {props.typeDefs[0].defs[index].map((def, i) => (
-              <li key={i}>{def}</li>
+              <li key={uuidv4()}>{def}</li>
             ))}
           </ul>
         </div>
